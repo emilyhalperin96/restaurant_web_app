fa69807e4e75a77b38b1e668f14cd4356506b2c7
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-09-06 15:22:36 -0400

    added items into template

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('meal', 'status')
+    list_filter = ('status', ) #what you're going to filter by
+    search_fields = ('meal', 'descirption') #what you're going to search by 
+
+admin.site.register(Item, MenuItemAdmin)
+
+
